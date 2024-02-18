6266c47ab0064a55f49eb8e5998c7e8c7d252382
mboiteau <maxime.boiteau@epitech.eu>
2024-02-18 18:11:17 +0100

    create first section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,31 +1,68 @@
+import React from "react";
+
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 import Navbar from "../components/Navbar";
 
-const Home = () => {
-  const t = useTranslations("Index");
+const Home: React.FC = () => {
+  const t = useTranslations();
 
   return (
-    <div>
+    <div className="container mx-auto">
       <Navbar />
-      <div className="flex flex-col">
-        <h1>{t("title")}</h1>
-        <div>
-          <Link locale="es" href="/es">
-            Español
-          </Link>
+
+      <main>
+        <section id="main" className="bg-blue-500 text-white py-20">
+          <div className="container mx-auto flex flex-col md:flex-row items-center justify-center">
+            <div className="text-center md:pl-12">
+              <h1 className="text-5xl font-bold">{t("main.title")}</h1>
+              <div className="mt-10">
+                <button className="mr-4 bg-white text-blue-500 px-5 py-3 rounded-lg">
+                  {t("main.contactButton")}
+                </button>
+                <button className="bg-white text-blue-500 px-5 py-3 rounded-lg">
+                  {t("main.ourServicesButton")}
+                </button>
+              </div>
+              <p className="mt-10">{t("main.subtitle")}</p>
+            </div>
+            <img
+              src="/images/astronaut_soldier.png"
+              alt="Astronaut Soldier"
+              className="max-w-2xl"
+            />
+          </div>
+        </section>
+
+        <section id="about" className="py-16">
+          <div className="container mx-auto">
+            <h2 className="text-2xl font-bold">{t("about")}</h2>
+            <p className="mt-4">{t("about.description")}</p>
+          </div>
+        </section>
+
+        <section id="services" className="bg-gray-200 py-16">
+          <div className="container mx-auto">
+            <h2 className="text-2xl font-bold">{t("services")}</h2>
+            <button className="mt-8 bg-white text-blue-500 px-4 py-2 rounded-lg">
+              {t("main.ourServicesButton")}
+            </button>
+          </div>
+        </section>
+
+        <section id="contact" className="py-16">
+          <div className="container mx-auto">
+            <h2 className="text-2xl font-bold">{t("contact")}</h2>
+          </div>
+        </section>
+      </main>
+
+      <footer className="py-6 bg-blue-500 text-white">
+        <div className="container mx-auto">
+          <p>
+            &copy; {new Date().getFullYear()} {t("title")}
+          </p>
         </div>
-        <div>
-          <Link locale="en" href="/en">
-            English
-          </Link>
-        </div>
-        <div>
-          <Link locale="fr" href="/fr">
-            Français
-          </Link>
-        </div>
-      </div>
+      </footer>
     </div>
   );
 };
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,37 +2,47 @@ import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 const Navbar = () => {
-  const t = useTranslations("navbar");
+  const t = useTranslations();
 
   return (
-    <nav className="dark:bg-gray-900">
-      <ul className="flex p-4 space-x-10">
-        <li>
-          <Link
-            href="/"
-            className="bg-blue-700 bg-transparent text-blue-700 hover:text-gray-700"
-          >
-            {t("home")}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/aboutus"
-            className="hover:bg-transparent hover:text-gray-700"
-          >
-            {t("aboutus")}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/blog"
-            className="hover:bg-transparent hover:text-gray-700"
-          >
-            {t("blog")}
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="py-6">
+      <nav className="text-center">
+        <ul className="flex p-4 space-x-10">
+          <li>
+            <Link
+              href="#main"
+              className="hover:bg-transparent hover:text-blue-500"
+            >
+              LOGO
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#about"
+              className="hover:bg-transparent hover:text-blue-500"
+            >
+              {t("about")}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#services"
+              className="hover:bg-transparent hover:text-blue-500"
+            >
+              {t("services")}
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#contact"
+              className="hover:bg-transparent hover:text-blue-500"
+            >
+              {t("contact")}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
