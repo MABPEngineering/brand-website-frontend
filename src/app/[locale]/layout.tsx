c48b573dbbe89718cfbbbf0b3dc8a7490599e7a8
mboiteau <maxime.boiteau@epitech.eu>
2024-02-23 12:09:16 +0100

    add Avenir font next

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,35 @@
-import { getTranslations } from 'next-intl/server';
-import './globals.css';
+import { getTranslations } from "next-intl/server";
+import localFont from "next/font/local";
+
+const avenir = localFont({
+  src: [
+    {
+      path: "../../../public/fonts/AvenirBlack.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/AvenirBook.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/AvenirHeavy.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/AvenirLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/AvenirRegular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+});
 
 export async function generateMetadata({
   params: { locale },
@@ -9,8 +39,8 @@ export async function generateMetadata({
   const t = await getTranslations({ locale });
 
   return {
-    title: t('title'),
-    description: t('description'),
+    title: t("title"),
+    description: t("description"),
   };
 }
 
@@ -22,7 +52,7 @@ const LocaleLayout = ({
   params: { locale: string };
 }) => {
   return (
-    <html lang={locale}>
+    <html lang={locale} className={avenir.className}>
       <body>{children}</body>
     </html>
   );
