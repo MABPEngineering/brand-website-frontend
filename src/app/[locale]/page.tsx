483caa8cab4ed00df569a0f4dd909e1768eae630
mboiteau <maxime.boiteau@epitech.eu>
2024-02-28 21:41:37 +0100

    feature: solved translation issue for client component Navbar + add prettier config

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,26 @@
+import { useTranslations } from 'next-intl'
 
-import BodyWrapper from '../components/BodyWrapper';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import Services from '../components/Services';
+import BodyWrapper from '../components/BodyWrapper'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+import Services from '../components/Services'
 
 const Home = () => {
+  const t = useTranslations('navbar')
+
+  const sections = [
+    { title: t('home'), id: 'home' },
+    { title: t('services'), id: 'services' },
+    { title: t('blog'), id: 'blog' },
+    { title: t('contactUs'), id: 'contactUs' },
+  ]
   return (
     <BodyWrapper>
-      <Navbar />
+      <Navbar sections={sections} />
       <Header />
       <Services />
     </BodyWrapper>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,57 +1,52 @@
-"use client";
+'use client'
 
-import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
+import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
 
-const Navbar = () => {
-  const prevScrollPos = useRef(0);
-  const [visible, setVisible] = useState(true);
-  const [showShadow, setShowShadow] = useState(false);
+const Navbar = ({
+  sections,
+}: {
+  sections: { title: string; id: string }[]
+}) => {
+  const prevScrollPos = useRef(0)
+  const [visible, setVisible] = useState(true)
+  const [showShadow, setShowShadow] = useState(false)
 
   const handleScroll = () => {
-    const currentScrollPos = window.scrollY;
+    const currentScrollPos = window.scrollY
     if (currentScrollPos > 50) {
-      setShowShadow(true);
+      setShowShadow(true)
+    } else {
+      setShowShadow(false)
     }
-    else {
-      setShowShadow(false);
-    }
-    const scrollingDown = currentScrollPos > prevScrollPos.current;
-    setVisible(!scrollingDown);
-    prevScrollPos.current = currentScrollPos;
-  };
+    const scrollingDown = currentScrollPos > prevScrollPos.current
+    setVisible(!scrollingDown)
+    prevScrollPos.current = currentScrollPos
+  }
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <nav
-      className={`transition duration-300 ease-in-out ${visible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
-        } fixed top-0 left-0 right-0 z-50 bg-white ${showShadow ? 'shadow-md' : ''}`}
+      className={`transition duration-300 ease-in-out ${
+        visible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
+      } fixed top-0 left-0 right-0 z-50 bg-white ${
+        showShadow ? 'shadow-md' : ''
+      }`}
     >
       <ul className="flex p-4 space-x-10">
-        <li>
-          <Link
-            href="#main"
-          >
-            Home
-            {/* {t('home')} */}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="#services"
-          >
-            Services
-            {/* {t('services')} */}
-          </Link>
-        </li>
+        {sections.map(({ title, id }) => (
+          <li key={id}>
+            <Link href={`#${id}`}>{title}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
