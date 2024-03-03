946699b7810aecea52bed13e0e6d201d81025233
mboiteau <maxime.boiteau@epitech.eu>
2024-03-03 21:25:55 +0100

    feature: add mobile navbar and enhance style navbar desktop

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,30 @@
-import { useTranslations } from 'next-intl'
+import { useTranslations } from 'next-intl';
 
-import BodyWrapper from '../components/BodyWrapper'
-import Header from '../components/Header'
-import Navbar from '../components/Navbar'
-import Services from '../components/Services'
+import blogIcon from '../../../public/navbar-icons/blog-icon.png';
+import emailIcon from '../../../public/navbar-icons/email-icon.png';
+import homeIcon from '../../../public/navbar-icons/home-icon.png';
+import trophyIcon from '../../../public/navbar-icons/trophy-icon.png';
+import BodyWrapper from '../components/BodyWrapper';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import Services from '../components/Services';
 
 const Home = () => {
-  const t = useTranslations('navbar')
+  const t = useTranslations('navbar');
 
   const sections = [
-    { title: t('home'), id: 'home' },
-    { title: t('services'), id: 'services' },
-    { title: t('blog'), id: 'blog' },
-    { title: t('contactUs'), id: 'contactUs' },
-  ]
+    { title: t('home'), id: 'home', icon: homeIcon },
+    { title: t('services'), id: 'services', icon: trophyIcon },
+    { title: t('blog'), id: 'blog', icon: emailIcon },
+    { title: t('contactUs'), id: 'contactUs', icon: blogIcon },
+  ];
   return (
     <BodyWrapper>
       <Navbar sections={sections} />
       <Header />
       <Services />
     </BodyWrapper>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,52 +1,57 @@
-'use client'
+'use client';
 
-import Link from 'next/link'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react';
+
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
 
 const Navbar = ({
   sections,
 }: {
-  sections: { title: string; id: string }[]
+  sections: { title: string; id: string; icon: StaticImageData }[];
 }) => {
-  const prevScrollPos = useRef(0)
-  const [visible, setVisible] = useState(true)
-  const [showShadow, setShowShadow] = useState(false)
+  const prevScrollPos = useRef(0);
+  const [visible, setVisible] = useState(true);
 
   const handleScroll = () => {
-    const currentScrollPos = window.scrollY
-    if (currentScrollPos > 50) {
-      setShowShadow(true)
-    } else {
-      setShowShadow(false)
-    }
-    const scrollingDown = currentScrollPos > prevScrollPos.current
-    setVisible(!scrollingDown)
-    prevScrollPos.current = currentScrollPos
-  }
+    const currentScrollPos = window.scrollY;
+    const scrollingDown = currentScrollPos > prevScrollPos.current;
+    setVisible(!scrollingDown);
+    prevScrollPos.current = currentScrollPos;
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <nav
-      className={`transition duration-300 ease-in-out ${
-        visible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
-      } fixed top-0 left-0 right-0 z-50 bg-white ${
-        showShadow ? 'shadow-md' : ''
-      }`}
+      className={` transition duration-300 ease-in-out ${
+        visible
+          ? 'md:translate-y-0 md:opacity-100'
+          : 'md:-translate-y-full md:opacity-0'
+      } fixed bottom-0 md:top-0 md:bottom-auto left-0 right-0 z-50 shadow-md bg-white`}
     >
-      <ul className="flex p-4 space-x-10">
-        {sections.map(({ title, id }) => (
+      <ul className="flex p-3 justify-around md:justify-center md:gap-20">
+        {sections.map(({ title, id, icon }) => (
           <li key={id}>
-            <Link href={`#${id}`}>{title}</Link>
+            <Link href={`#${id}`}>
+              <span className="hidden font-bold md:inline hover:underline underline-offset-8 decoration-2">
+                {title}
+              </span>
+              <Image
+                src={icon}
+                alt={`${title} icon`}
+                className="md:hidden size-8"
+              />
+            </Link>
           </li>
         ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
